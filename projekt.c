3a65fc47472d3693ad9f8fd4154733a931efe0cc
Kajzerooo <148884896+Kajzerooo@users.noreply.github.com>
2023-10-24 17:45:32 +0200

    Add files via upload

--- projekt.c
+++ projekt.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <dvec.h>
 
-//Generowanie liczb pseudo-losowych do pliku (uzupełnij ścieżkę!)
+//-----Generowanie liczb pseudo-losowych do pliku (uzupełnij ścieżkę!)-----
 void Generuj() {
 
     //Ścieżka do pliku
@@ -44,35 +43,32 @@ void Generuj() {
     printf("\n");
 }
 
-//Odczytywanie liczb z powstałego pliku tekstowego.
-int* Odczytaj() {
-    const char* nazwa_pliku = "C://Users/artex/downloads/liczby.txt";
-    int* liczba_liczb;
-    FILE* plik = fopen(nazwa_pliku, "r");
-    
+//------Odczytywanie liczb z powstałego pliku tekstowego.----- [NIE DZIAŁA]
+#define MAX_LICZBY 5000000 // Maksymalna liczba liczb w pliku tekstowym
+int Odczytaj(const char *nazwaPliku, int tablica[], int *iloscLiczb) {
+    nazwaPliku = "C://Users/artex/downloads/liczby.txt";
+    FILE *plik;
+    plik = fopen(nazwaPliku, "r");
     if (plik == NULL) {
-        printf("Nie można otworzyć pliku do odczytu.\n");
-        return NULL;
+        perror("Błąd przy otwieraniu pliku");
+        return 0;
     }
 
-    int* tablica_liczb = NULL;
-    int liczba;
-    int indeks = 0;
+    int liczba, i = 0;
 
     while (fscanf(plik, "%d", &liczba) == 1) {
-        if (tablica_liczb == NULL) {
-            tablica_liczb = (int*)malloc(sizeof(int));
-        } else {
-            tablica_liczb = (int*)realloc(tablica_liczb, (indeks + 1) * sizeof(int));
+        if (i >= MAX_LICZBY) {
+            fprintf(stderr, "Osiągnięto maksymalną liczbę liczb w pliku\n");
+            break;
         }
-
-        tablica_liczb[indeks] = liczba;
-        indeks++;
+        tablica[i] = liczba;
+        i++;
     }
 
+    *iloscLiczb = i; // Przekazujemy ilość odczytanych liczb przez wskaźnik
+
     fclose(plik);
-    *liczba_liczb = indeks;
-    return tablica_liczb;
+    return 1; // Zwracamy 1, aby oznaczyć sukces
 }
 
 //-----Funkcja sortowania bombelkowego----- [DZIAŁA]
@@ -94,7 +90,7 @@ void BubbleSort(int tab_a[], int a) {
 }
 
 
-//-----Funkcja sortowania Shell Sort-----
+//-----Funkcja sortowania Shell Sort----- [DZIAŁA]
 void ShellSort(int Tab_a[], int b) {
     // Przearanżowuje elementy w interwałach kolejno: b/2, b/4, b/8
     int interval;
@@ -110,36 +106,84 @@ void ShellSort(int Tab_a[], int b) {
     }
 }
 
+//-----Funkcja sortowania Quick Sort----- [IN PROGRESS]
+    // Funkcja zamieniająca elementy tablicy
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Funkcja partition do wybierania pivota i dzielenia tablicy
+int partition(int tab_c[], int low, int high) {
+    int pivot = tab_c[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (tab_c[j] < pivot) {
+            i++;
+            swap(&tab_c[i], &tab_c[j]);
+        }
+    }
+    swap(&tab_c[i + 1], &tab_c[high]);
+    return (i + 1);
+}
+
+// Główna funkcja QuickSort
+void QuickSort(int tab_c[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(tab_c, low, high);
+
+        QuickSort(tab_c, low, pivotIndex - 1);
+        QuickSort(tab_c, pivotIndex + 1, high);
+    }
+}
 
 int main() {
+    FILE *file = fopen("C://Users/artex/downloads/liczby.txt", "r");
+    if (file == NULL) {
+        printf("Nie można otworzyć pliku.\n");
+        return 1;
+    }
+
     const char* nazwa_pliku = "liczby.txt";
     int tab_a[] = {12, 432, 110, 25, 443, 80, 8080}; //Wewnątrz tablicy prawdopodobnie będzie odnośnik do pliku z liczbami.
     int tab_b[] = {34, 13, 727, 17, 69, 420, 2137}; //Wewnątrz tablicy prawdopodobnie będzie odnośnik do pliku z liczbami.
+    int tab_c[] = {16, 54, 88, 96, 1, 3, 1910};
+
 
     int a = sizeof(tab_a) / sizeof(tab_a[0]);
     int b = sizeof(tab_b) / sizeof(tab_b[0]);
+    int c = sizeof(tab_c) / sizeof(tab_c[0]);
 
     Generuj();
     //Odczytaj();
-    BubbleSort(tab_a, a);
-    ShellSort(tab_b, b);
     
-    //Print posortowanego zbioru BubbleSort
-    printf("Bubble Sorted array: ");
-    printf("\n");
+//-------------------------------------------------- 
 
-    for (int i = 0; i < a; i++) {
+    //BUBBLE SORT
+    BubbleSort(tab_a, a);
+    printf("Bubble Sorted array: \n");
+    for (int i = 0; i < a; i++){
         printf("%d ", tab_a[i]);
     }
-    //Koniec printu posortowanego zbioru Shell Sort
 
-    printf("\n");
-    printf("\n");
-    printf("Shell Sorted array: ");
-    printf("\n");
+//--------------------------------------------------
 
-    for (int i = 0; i < b; i++) {
+    //SHELL SORT
+    ShellSort(tab_b, b);
+    printf("\n\nShell Sorted array: \n");
+    for (int i = 0; i < b; i++){
         printf("%d ", tab_b[i]);
+    }
+
+//-------------------------------------------------- 
+
+    //QUICK SORT
+    QuickSort(tab_c, 0, c-1);
+    printf("\n\nQuick Sorted array: \n");
+    for (int i = 0; i < b; i++){
+        printf("%d ", tab_c[i]);
     }
 
     return 0;
